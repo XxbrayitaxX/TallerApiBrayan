f3f4cfb9e7873e1e852599b200748e5baee35192
Sena <dimatesebas73@gmail.com>
2025-04-24 13:03:50 -0500

    Layouts estructura completa

---
+++ src/Layouts/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    <footer className=''>
+        <p> brayan aguirre © 2025 - Todos los derechos reservados</p>
+    </footer>
+    </>
+  )
+}
+
+export default Footer
